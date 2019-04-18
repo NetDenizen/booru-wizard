efb415f531979bd34a745669b26564eeaba7a745
Net Denizen <NetDenizen@users.noreply.github.com>
2019-04-18 01:18:57 -0400

    Round timer precision to milliseconds.

--- booruwizard/ui/image.py
+++ booruwizard/ui/image.py
@@ -218,10 +218,10 @@ class ImagePanel(wx.Panel):
 	def _OnFileUpdateTick(self, message, arg2=None):
 		MessageSeconds = message / 1000000.0
 		minutes = str( int(MessageSeconds // 60.0) )
-		FullSeconds = str(MessageSeconds % 60.0)
+		FullSeconds = str( round(MessageSeconds % 60.0, 3) )
 		SplitSeconds = FullSeconds.split('.', 2)
-		output = ''.join( ( 'Until Next Flush: ',
-							minutes.zfill(2), ':', SplitSeconds[0].zfill(2), '.', SplitSeconds[1].zfill(4) ) )
+		output = ''.join( ('Until Next Flush: ',
+						   minutes.zfill(2), ':', SplitSeconds[0].zfill(2), '.', SplitSeconds[1]) )
 		wx.CallAfter(self.OutputUpdateTimer.SetLabel, output)
 	def _OnImageQualityHigh21(self, message, arg2=None):
 		"Set image quality to high 2+1 (box average on downscale, bicubic on upscale), update the radio button, and repaint the image."
