9bba9794d0d8f3c0abd66a60f841fedbdd3bc81e
NetDenizen <NetDenizen@users.noreply.github.com>
2019-04-27 11:05:46 -0400

    Add question search keybinds.

--- booruwizard/lib/keyhandler.py
+++ booruwizard/lib/keyhandler.py
@@ -11,6 +11,8 @@ ID_FOCUS_IMAGE_INDEX = wx.NewId()
 ID_FOCUS_PATH_NAME = wx.NewId()
 ID_FOCUS_PATH_NAME_MENU = wx.NewId()
 ID_FOCUS_QUESTION_INDEX = wx.NewId()
+ID_FOCUS_QUESTION_SEARCH = wx.NewId()
+ID_FOCUS_QUESTION_SEARCH_MENU = wx.NewId()
 ID_FOCUS_PROMPT_BODY = wx.NewId()
 ID_FOCUS_QUESTION_BODY = wx.NewId()
 ID_IMAGE_QUALITY_LEFT = wx.NewId()
@@ -29,62 +31,66 @@ ID_ZOOM_OUT = wx.NewId()
 ID_ZOOM_FIT = wx.NewId()
 ID_ZOOM_ACTUAL_SIZE = wx.NewId()
 KEYBIND_IDS = {
-	'exit'                   : ID_EMERGENCY_EXIT        ,
-	'flush_changes'          : ID_FILE_UPDATE_FORCE     ,
-	'left_image'             : ID_LEFT_IMAGE            ,
-	'right_image'            : ID_RIGHT_IMAGE           ,
-	'left_question'          : ID_LEFT_QUESTION         ,
-	'right_question'         : ID_RIGHT_QUESTION        ,
-	'select_image_index'     : ID_FOCUS_IMAGE_INDEX     ,
-	'select_image_path'      : ID_FOCUS_PATH_NAME       ,
-	'select_image_path_menu' : ID_FOCUS_PATH_NAME_MENU  ,
-	'select_question_index'  : ID_FOCUS_QUESTION_INDEX  ,
-	'select_instructions'    : ID_FOCUS_PROMPT_BODY     ,
-	'select_question'        : ID_FOCUS_QUESTION_BODY   ,
-	'image_quality_left'     : ID_IMAGE_QUALITY_LEFT    ,
-	'image_quality_right'    : ID_IMAGE_QUALITY_RIGHT   ,
-	'image_quality_high_2_1' : ID_IMAGE_QUALITY_HIGH_2_1,
-	'image_quality_high_2'   : ID_IMAGE_QUALITY_HIGH_2  ,
-	'image_quality_high_1'   : ID_IMAGE_QUALITY_HIGH_1  ,
-	'image_quality_medium'   : ID_IMAGE_QUALITY_MEDIUM  ,
-	'image_quality_low'      : ID_IMAGE_QUALITY_LOW     ,
-	'pan_left'               : ID_PAN_LEFT              ,
-	'pan_right'              : ID_PAN_RIGHT             ,
-	'pan_up'                 : ID_PAN_UP                ,
-	'pan_down'               : ID_PAN_DOWN              ,
-	'zoom_in'                : ID_ZOOM_IN               ,
-	'zoom_out'               : ID_ZOOM_OUT              ,
-	'zoom_fit'               : ID_ZOOM_FIT              ,
-	'zoom_actual_size'       : ID_ZOOM_ACTUAL_SIZE
+	'exit'                        : ID_EMERGENCY_EXIT             ,
+	'flush_changes'               : ID_FILE_UPDATE_FORCE          ,
+	'left_image'                  : ID_LEFT_IMAGE                 ,
+	'right_image'                 : ID_RIGHT_IMAGE                ,
+	'left_question'               : ID_LEFT_QUESTION              ,
+	'right_question'              : ID_RIGHT_QUESTION             ,
+	'select_image_index'          : ID_FOCUS_IMAGE_INDEX          ,
+	'select_image_path'           : ID_FOCUS_PATH_NAME            ,
+	'select_image_path_menu'      : ID_FOCUS_PATH_NAME_MENU       ,
+	'select_question_index'       : ID_FOCUS_QUESTION_INDEX       ,
+	'select_question_search'      : ID_FOCUS_QUESTION_SEARCH      ,
+	'select_question_search_menu' : ID_FOCUS_QUESTION_SEARCH_MENU ,
+	'select_instructions'         : ID_FOCUS_PROMPT_BODY          ,
+	'select_question'             : ID_FOCUS_QUESTION_BODY        ,
+	'image_quality_left'          : ID_IMAGE_QUALITY_LEFT         ,
+	'image_quality_right'         : ID_IMAGE_QUALITY_RIGHT        ,
+	'image_quality_high_2_1'      : ID_IMAGE_QUALITY_HIGH_2_1     ,
+	'image_quality_high_2'        : ID_IMAGE_QUALITY_HIGH_2       ,
+	'image_quality_high_1'        : ID_IMAGE_QUALITY_HIGH_1       ,
+	'image_quality_medium'        : ID_IMAGE_QUALITY_MEDIUM       ,
+	'image_quality_low'           : ID_IMAGE_QUALITY_LOW          ,
+	'pan_left'                    : ID_PAN_LEFT                   ,
+	'pan_right'                   : ID_PAN_RIGHT                  ,
+	'pan_up'                      : ID_PAN_UP                     ,
+	'pan_down'                    : ID_PAN_DOWN                   ,
+	'zoom_in'                     : ID_ZOOM_IN                    ,
+	'zoom_out'                    : ID_ZOOM_OUT                   ,
+	'zoom_fit'                    : ID_ZOOM_FIT                   ,
+	'zoom_actual_size'            : ID_ZOOM_ACTUAL_SIZE
 }
 KEYBIND_MESSAGES = {
-	ID_EMERGENCY_EXIT         : 'EmergencyExit'      ,
-	ID_FILE_UPDATE_FORCE      : 'FileUpdateForce'    ,
-	ID_LEFT_IMAGE             : 'LeftImage'          ,
-	ID_RIGHT_IMAGE            : 'RightImage'         ,
-	ID_LEFT_QUESTION          : 'LeftQuestion'       ,
-	ID_RIGHT_QUESTION         : 'RightQuestion'      ,
-	ID_FOCUS_IMAGE_INDEX      : 'FocusImageIndex'    ,
-	ID_FOCUS_PATH_NAME        : 'FocusPathName'      ,
-	ID_FOCUS_PATH_NAME_MENU   : 'FocusPathNameMenu'  ,
-	ID_FOCUS_QUESTION_INDEX   : 'FocusQuestionIndex' ,
-	ID_FOCUS_PROMPT_BODY      : 'FocusPromptBody'    ,
-	ID_FOCUS_QUESTION_BODY    : 'FocusQuestionBody'  ,
-	ID_IMAGE_QUALITY_LEFT     : 'ImageQualityLeft'   ,
-	ID_IMAGE_QUALITY_RIGHT    : 'ImageQualityRight'  ,
-	ID_IMAGE_QUALITY_HIGH_2_1 : 'ImageQualityHigh21' ,
-	ID_IMAGE_QUALITY_HIGH_2   : 'ImageQualityHigh2'  ,
-	ID_IMAGE_QUALITY_HIGH_1   : 'ImageQualityHigh1'  ,
-	ID_IMAGE_QUALITY_MEDIUM   : 'ImageQualityMedium' ,
-	ID_IMAGE_QUALITY_LOW      : 'ImageQualityLow'    ,
-	ID_PAN_LEFT               : 'PanLeft'            ,
-	ID_PAN_RIGHT              : 'PanRight'           ,
-	ID_PAN_UP                 : 'PanUp'              ,
-	ID_PAN_DOWN               : 'PanDown'            ,
-	ID_ZOOM_IN                : 'ZoomIn'             ,
-	ID_ZOOM_OUT               : 'ZoomOut'            ,
-	ID_ZOOM_FIT               : 'ZoomFit'            ,
-	ID_ZOOM_ACTUAL_SIZE       : 'ZoomActualSize'
+	ID_EMERGENCY_EXIT              : 'EmergencyExit'          ,
+	ID_FILE_UPDATE_FORCE           : 'FileUpdateForce'        ,
+	ID_LEFT_IMAGE                  : 'LeftImage'              ,
+	ID_RIGHT_IMAGE                 : 'RightImage'             ,
+	ID_LEFT_QUESTION               : 'LeftQuestion'           ,
+	ID_RIGHT_QUESTION              : 'RightQuestion'          ,
+	ID_FOCUS_IMAGE_INDEX           : 'FocusImageIndex'        ,
+	ID_FOCUS_PATH_NAME             : 'FocusPathName'          ,
+	ID_FOCUS_PATH_NAME_MENU        : 'FocusPathNameMenu'      ,
+	ID_FOCUS_QUESTION_INDEX        : 'FocusQuestionIndex'     ,
+	ID_FOCUS_QUESTION_SEARCH       : 'FocusQuestionSearch'    ,
+	ID_FOCUS_QUESTION_SEARCH_MENU  : 'FocusQuestionSearchMenu',
+	ID_FOCUS_PROMPT_BODY           : 'FocusPromptBody'        ,
+	ID_FOCUS_QUESTION_BODY         : 'FocusQuestionBody'      ,
+	ID_IMAGE_QUALITY_LEFT          : 'ImageQualityLeft'       ,
+	ID_IMAGE_QUALITY_RIGHT         : 'ImageQualityRight'      ,
+	ID_IMAGE_QUALITY_HIGH_2_1      : 'ImageQualityHigh21'     ,
+	ID_IMAGE_QUALITY_HIGH_2        : 'ImageQualityHigh2'      ,
+	ID_IMAGE_QUALITY_HIGH_1        : 'ImageQualityHigh1'      ,
+	ID_IMAGE_QUALITY_MEDIUM        : 'ImageQualityMedium'     ,
+	ID_IMAGE_QUALITY_LOW           : 'ImageQualityLow'        ,
+	ID_PAN_LEFT                    : 'PanLeft'                ,
+	ID_PAN_RIGHT                   : 'PanRight'               ,
+	ID_PAN_UP                      : 'PanUp'                  ,
+	ID_PAN_DOWN                    : 'PanDown'                ,
+	ID_ZOOM_IN                     : 'ZoomIn'                 ,
+	ID_ZOOM_OUT                    : 'ZoomOut'                ,
+	ID_ZOOM_FIT                    : 'ZoomFit'                ,
+	ID_ZOOM_ACTUAL_SIZE            : 'ZoomActualSize'
 }
 
 class KeyHandlerError(Exception):
@@ -100,8 +106,10 @@ class KeyHandler(wx.Object):
 		self.RightQuestionItem = wx.MenuItem(id=wx.NewId(), text="RightQuestion", helpString="Switch to the 'right' or next image.")
 		self.FocusImageIndexItem = wx.MenuItem(id=wx.NewId(), text="FocusImageIndex", helpString="Focus on the image index entry.")
 		self.FocusImagePathItem = wx.MenuItem(id=wx.NewId(), text="FocusPathName", helpString="Focus on the image path name.")
-		self.FocusImagePathMenuItem = wx.MenuItem(id=wx.NewId(), text="FocusPathNameMenu", helpString="Focus on the image path name menu.")
+		self.FocusImagePathMenuItem = wx.MenuItem(id=wx.NewId(), text="FocusPathNameMenu", helpString="Open and focus on the image path name menu.")
 		self.FocusQuestionIndexItem = wx.MenuItem(id=wx.NewId(), text="FocusQuestionIndex", helpString="Focus on the question index entry.")
+		self.FocusQuestionSearchItem = wx.MenuItem(id=wx.NewId(), text="FocusQuestionSearch", helpString="Focus on the question search entry.")
+		self.FocusQuestionSearchMenuItem = wx.MenuItem(id=wx.NewId(), text="FocusQuestionSearchMenu", helpString="Open and focus on the question search menu.")
 		self.FocusInstructionsItem = wx.MenuItem(id=wx.NewId(), text="FocusPromptBody", helpString="Focus on the prompt text.")
 		self.FocusQuestionsItem = wx.MenuItem(id=wx.NewId(), text="FocusQuestionBody", helpString="Focus on the question field.")
 		self.ImageQualityLeftItem = wx.MenuItem(id=wx.NewId(), text="ImageQualityLeft", helpString="Cycle through image quality from low to high.")
@@ -119,33 +127,35 @@ class KeyHandler(wx.Object):
 		self.ZoomOutItem = wx.MenuItem(id=wx.NewId(), text="ZoomOut", helpString="Zoom out image.")
 		self.ZoomFitItem = wx.MenuItem(id=wx.NewId(), text="ZoomFit", helpString="Zoom image to fit perfectly in the window.")
 		self.ZoomActualSizeItem = wx.MenuItem(id=wx.NewId(), text="ZoomActualSize", helpString="Zoom image so it is displayed at actual size, whether or not it fits in the window.")
-		self.MenuItems = {'exit'                   : self.EmergencyExitItem      ,
-						  'flush_changes'          : self.FileUpdateForceItem    ,
-						  'left_image'             : self.LeftImageItem          ,
-						  'right_image'            : self.RightImageItem         ,
-						  'left_question'          : self.LeftQuestionItem       ,
-						  'right_question'         : self.RightQuestionItem      ,
-						  'select_image_index'     : self.FocusImageIndexItem    ,
-						  'select_image_path'      : self.FocusImagePathItem     ,
-						  'select_image_path_menu' : self.FocusImagePathMenuItem ,
-						  'select_question_index'  : self.FocusQuestionIndexItem ,
-						  'select_instructions'    : self.FocusInstructionsItem  ,
-						  'select_question'        : self.FocusQuestionsItem     ,
-						  'image_quality_left'     : self.ImageQualityLeftItem   ,
-						  'image_quality_right'    : self.ImageQualityRightItem  ,
-						  'image_quality_high_2_1' : self.ImageQualityHigh21Item ,
-						  'image_quality_high_2'   : self.ImageQualityHigh2Item  ,
-						  'image_quality_high_1'   : self.ImageQualityHigh1Item  ,
-						  'image_quality_medium'   : self.ImageQualityMediumItem ,
-						  'image_quality_low'      : self.ImageQualityLowItem    ,
-						  'pan_left'               : self.PanLeftItem            ,
-						  'pan_right'              : self.PanRightItem           ,
-						  'pan_up'                 : self.PanUpItem              ,
-						  'pan_down'               : self.PanDownItem            ,
-						  'zoom_in'                : self.ZoomInItem             ,
-						  'zoom_out'               : self.ZoomOutItem            ,
-						  'zoom_fit'               : self.ZoomFitItem            ,
-						  'zoom_actual_size'       : self.ZoomActualSizeItem
+		self.MenuItems = {'exit'                        : self.EmergencyExitItem                ,
+						  'flush_changes'               : self.FileUpdateForceItem              ,
+						  'left_image'                  : self.LeftImageItem                    ,
+						  'right_image'                 : self.RightImageItem                   ,
+						  'left_question'               : self.LeftQuestionItem                 ,
+						  'right_question'              : self.RightQuestionItem                ,
+						  'select_image_index'          : self.FocusImageIndexItem              ,
+						  'select_image_path'           : self.FocusImagePathItem               ,
+						  'select_image_path_menu'      : self.FocusImagePathMenuItem           ,
+						  'select_question_index'       : self.FocusQuestionIndexItem           ,
+						  'select_question_search'      : self.FocusQuestionSearchItem          ,
+						  'select_question_search_menu' : self.FocusQuestionSearchMenuItem      ,
+						  'select_instructions'         : self.FocusInstructionsItem            ,
+						  'select_question'             : self.FocusQuestionsItem               ,
+						  'image_quality_left'          : self.ImageQualityLeftItem             ,
+						  'image_quality_right'         : self.ImageQualityRightItem            ,
+						  'image_quality_high_2_1'      : self.ImageQualityHigh21Item           ,
+						  'image_quality_high_2'        : self.ImageQualityHigh2Item            ,
+						  'image_quality_high_1'        : self.ImageQualityHigh1Item            ,
+						  'image_quality_medium'        : self.ImageQualityMediumItem           ,
+						  'image_quality_low'           : self.ImageQualityLowItem              ,
+						  'pan_left'                    : self.PanLeftItem                      ,
+						  'pan_right'                   : self.PanRightItem                     ,
+						  'pan_up'                      : self.PanUpItem                        ,
+						  'pan_down'                    : self.PanDownItem                      ,
+						  'zoom_in'                     : self.ZoomInItem                       ,
+						  'zoom_out'                    : self.ZoomOutItem                      ,
+						  'zoom_fit'                    : self.ZoomFitItem                      ,
+						  'zoom_actual_size'            : self.ZoomActualSizeItem
 						 }
 		self.entries = []
 	def _OnEntry(self, e):
@@ -163,6 +173,8 @@ class KeyHandler(wx.Object):
 		obj.Bind(wx.EVT_MENU, self._OnEntry, id=ID_FOCUS_PATH_NAME)
 		obj.Bind(wx.EVT_MENU, self._OnEntry, id=ID_FOCUS_PATH_NAME_MENU)
 		obj.Bind(wx.EVT_MENU, self._OnEntry, id=ID_FOCUS_QUESTION_INDEX)
+		obj.Bind(wx.EVT_MENU, self._OnEntry, id=ID_FOCUS_QUESTION_SEARCH)
+		obj.Bind(wx.EVT_MENU, self._OnEntry, id=ID_FOCUS_QUESTION_SEARCH_MENU)
 		obj.Bind(wx.EVT_MENU, self._OnEntry, id=ID_FOCUS_PROMPT_BODY)
 		obj.Bind(wx.EVT_MENU, self._OnEntry, id=ID_FOCUS_QUESTION_BODY)
 		obj.Bind(wx.EVT_MENU, self._OnEntry, id=ID_IMAGE_QUALITY_LEFT)
