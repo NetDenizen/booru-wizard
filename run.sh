905936e2e44851dc9c422c01261daf4f86398e59
NetDenizen <NetDenizen@users.noreply.github.com>
2019-05-09 04:40:33 -0400

    Command line args for run.sh

--- run.sh
+++ run.sh
@@ -2,14 +2,24 @@
 
 # A POSIX-compliant shell-script to run the software with some default settings.
 EXE_PATH='./booru-wizard'
-SCHEMA='./default_schema.json'
 CONFIG='./default_config.cfg'
 IMAGE_INPUT=''
 JSON_INPUT=''
 JSON_OUTPUT=''
 
-"$EXE_PATH" --schema "$SCHEMA" \
-			--config "$CONFIG" \
+# extra_args can be set to include arguments which override those which came before it.
+# Should command line arguments be passed to this script, they will take the place of those.
+extra_args=''
+if [ $# -ne 0 ]
+then
+	DEFAULT_IFS=$IFS
+	IFS=' '
+	extra_args="$*"
+	IFS=$DEFAULT_IFS
+fi
+
+"$EXE_PATH" --config "$CONFIG" \
 			--image-input "$IMAGE_INPUT" \
 			--json-input "$JSON_INPUT" \
-			--json-output "$JSON_OUTPUT"
+			--json-output "$JSON_OUTPUT" \
+			$extra_args
