5266fd5d8520df041fccefd285d2c4a0c3c5543f
NetDenizen <NetDenizen@users.noreply.github.com>
2019-05-10 05:29:14 -0400

    Fix run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 :: A batch file to run the software with some default settings.
 @echo off
 
-set EXE_PATH="%cd%\booru-wizard"
+set EXE_PATH="%cd%\booru-wizard.exe"
 set CONFIG="%cd%\default_config.cfg"
 set IMAGE_INPUT=
 set JSON_INPUT=
@@ -10,10 +10,10 @@ set JSON_OUTPUT=
 :: extra_args can be set to include arguments which override those which came before it.
 :: Should command line arguments be passed to this script, they will take the place of those.
 set extra_args=
-if [%*] != [] set extra_args="%*"
+if not [%*] == [] set extra_args="%*"
 
 start "" "%EXE_PATH%" --config "%CONFIG%" ^
 					  --image-input "%IMAGE_INPUT%" ^
 					  --json-input "%JSON_INPUT%" ^
 					  --json-output "%JSON_OUTPUT%" ^
-					  "%extra_args%"
+					  %extra_args%
